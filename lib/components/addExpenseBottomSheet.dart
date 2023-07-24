885b5fcb97cb0f30be6f60e06f214eed7b13991b
adi_walture <adityawalture9298@gmail.com>
2023-07-25 00:00:01 +0530

    ui updated

--- lib/components/addExpenseBottomSheet.dart
+++ lib/components/addExpenseBottomSheet.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/components/expense.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class NewExpenses extends StatefulWidget {
   const NewExpenses({super.key, required this.onAddExpense});
@@ -81,103 +82,141 @@ class _NewExpensesState extends State<NewExpenses> {
   @override
   Widget build(BuildContext context) {
     final landScapeKeyboard = MediaQuery.of(context).viewInsets.bottom;
-    return SizedBox(
-      height: double.infinity,
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(16, 50, 16, landScapeKeyboard + 16),
-          child: Column(
-            children: [
-              TextField(
-                controller: _inputText,
-                maxLength: 50,
-                decoration: const InputDecoration(
-                  hintText: "Add a expense",
-                ),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      keyboardType: TextInputType.number,
-                      controller: _inputAmount,
-                      decoration: const InputDecoration(
-                        prefixText: "₹ ",
-                        hintText: "Amount",
+    return LayoutBuilder(builder: (ctx, constraints) {
+      //it configures ui according to the screen layout
+
+      // print(constraints.minWidth);
+      // print(constraints.maxWidth);
+      // print(constraints.minHeight);
+      // print(constraints.maxHeight);
+      final width = constraints.maxWidth;
+
+      return SizedBox(
+        height: double.infinity,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(16, 50, 16, landScapeKeyboard + 16),
+            child: Column(
+              children: [
+                if (width >= 600)
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: _inputText,
+                          maxLength: 50,
+                          decoration: const InputDecoration(
+                            hintText: "Add a expense",
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 24),
+                      Expanded(
+                        child: TextField(
+                          keyboardType: TextInputType.number,
+                          controller: _inputAmount,
+                          decoration: const InputDecoration(
+                            prefixText: "₹ ",
+                            hintText: "Amount",
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                else
+                  TextField(
+                    controller: _inputText,
+                    maxLength: 50,
+                    decoration: const InputDecoration(
+                      hintText: "Add a expense",
+                    ),
+                  ),
+                if(width >= 600)
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        controller: _inputAmount,
+                        decoration: const InputDecoration(
+                          prefixText: "₹ ",
+                          hintText: "Amount",
+                        ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        IconButton(
-                          onPressed: _datePicker,
-                          icon: const Icon(Icons.calendar_month_outlined),
-                        ),
-                        Text(
-                          _selectedDate == null
-                              ? "No Date Selected"
-                              : formatter.format(_selectedDate!),
-                        ),
-                      ],
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            onPressed: _datePicker,
+                            icon: const Icon(Icons.calendar_month_outlined),
+                          ),
+                          Text(
+                            _selectedDate == null
+                                ? "No Date Selected"
+                                : formatter.format(_selectedDate!),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 5),
-              Row(
-                children: [
-                  // const SizedBox(width: 5),
-                  DropdownButton(
-                      elevation: 8,
-                      borderRadius: BorderRadius.circular(4.0),
-                      hint: const Text("Type"),
-                      value: _selectedCategory,
-                      items: Category.values
-                          .map(
-                            (category) => DropdownMenuItem(
-                                value: category,
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    Icon(categoryIcons[category]),
-                                    //const SizedBox(width: 4),
-                                    Text(
-                                      category.name.toUpperCase(),
-                                    ),
-                                  ],
-                                )),
-                          )
-                          .toList(), //if the dropdown has a value selected it will change in set state
-                      onChanged: (value) {
-                        if (value == null) {
-                          return;
-                        }
-                        setState(() {
-                          _selectedCategory = value;
-                        });
-                      }),
-                  const Spacer(),
-                  ElevatedButton(
-                    onPressed: _addExpense,
-                    child: const Text("Add Expense"),
-                  )
-                ],
-              ),
-              const SizedBox(height: 15),
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text("Cancel"),
-              ),
-            ],
+                  ],
+                ),
+                const SizedBox(height: 5),
+                Row(
+                  children: [
+                    // const SizedBox(width: 5),
+                    DropdownButton(
+                        elevation: 8,
+                        borderRadius: BorderRadius.circular(4.0),
+                        hint: const Text("Type"),
+                        value: _selectedCategory,
+                        items: Category.values
+                            .map(
+                              (category) => DropdownMenuItem(
+                                  value: category,
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Icon(categoryIcons[category]),
+                                      //const SizedBox(width: 4),
+                                      Text(
+                                        category.name.toUpperCase(),
+                                      ),
+                                    ],
+                                  )),
+                            )
+                            .toList(), //if the dropdown has a value selected it will change in set state
+                        onChanged: (value) {
+                          if (value == null) {
+                            return;
+                          }
+                          setState(() {
+                            _selectedCategory = value;
+                          });
+                        }),
+                    const Spacer(),
+                    ElevatedButton(
+                      onPressed: _addExpense,
+                      child: const Text("Add Expense"),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 15),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Cancel"),
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
