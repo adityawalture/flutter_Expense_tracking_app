191bd0dc504bba66fd45162855d28aa56c714add
adi_walture <adityawalture9298@gmail.com>
2023-07-25 00:42:13 +0530

    platform ui updated

--- lib/components/addExpenseBottomSheet.dart
+++ lib/components/addExpenseBottomSheet.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:expense_tracker/components/expense.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -41,15 +44,24 @@ class _NewExpensesState extends State<NewExpenses> {
     });
   }
 
-  //function for adding expense to home pg
-  void _addExpense() {
-    final enteredAmount = double.tryParse(
-        _inputAmount.text); //tryParse will convert string into double
-    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
-    if (_inputText.text.trim().isEmpty ||
-        amountIsInvalid ||
-        _selectedDate == null) {
-      //show error
+  void _showDialog() {
+    if (Platform.isIOS) {
+      showCupertinoDialog(
+          context: context,
+          builder: (ctx) => CupertinoAlertDialog(
+                title: const Text("Invalid Input"),
+                content: const Text(
+                    "Please enter a valid title, date, amount and category"),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(ctx);
+                    },
+                    child: const Text("close"),
+                  ),
+                ],
+              ));
+    } else {
       showDialog(
         context: context,
         builder: (ctx) => AlertDialog(
@@ -66,6 +78,20 @@ class _NewExpensesState extends State<NewExpenses> {
           ],
         ),
       );
+    }
+  }
+
+  //function for adding expense to home pg
+  void _addExpense() {
+    final enteredAmount = double.tryParse(
+        _inputAmount.text); //tryParse will convert string into double
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    if (_inputText.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      //show error
+
+      _showDialog();
       return;
     }
     widget.onAddExpense(
@@ -132,79 +158,150 @@ class _NewExpensesState extends State<NewExpenses> {
                       hintText: "Add a expense",
                     ),
                   ),
-                if(width >= 600)
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        keyboardType: TextInputType.number,
-                        controller: _inputAmount,
-                        decoration: const InputDecoration(
-                          prefixText: "₹ ",
-                          hintText: "Amount",
+                if (width >= 600)
+                  Row(
+                    children: [
+                      DropdownButton(
+                          elevation: 8,
+                          borderRadius: BorderRadius.circular(4.0),
+                          hint: const Text("Type"),
+                          value: _selectedCategory,
+                          items: Category.values
+                              .map(
+                                (category) => DropdownMenuItem(
+                                    value: category,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Icon(categoryIcons[category]),
+                                        //const SizedBox(width: 4),
+                                        Text(
+                                          category.name.toUpperCase(),
+                                        ),
+                                      ],
+                                    )),
+                              )
+                              .toList(), //if the dropdown has a value selected it will change in set state
+                          onChanged: (value) {
+                            if (value == null) {
+                              return;
+                            }
+                            setState(() {
+                              _selectedCategory = value;
+                            });
+                          }),
+                      const SizedBox(width: 24),
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              onPressed: _datePicker,
+                              icon: const Icon(Icons.calendar_month_outlined),
+                            ),
+                            Text(
+                              _selectedDate == null
+                                  ? "No Date Selected"
+                                  : formatter.format(_selectedDate!),
+                            ),
+                          ],
                         ),
                       ),
-                    ),
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          IconButton(
-                            onPressed: _datePicker,
-                            icon: const Icon(Icons.calendar_month_outlined),
+                    ],
+                  )
+                else
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          keyboardType: TextInputType.number,
+                          controller: _inputAmount,
+                          decoration: const InputDecoration(
+                            prefixText: "₹ ",
+                            hintText: "Amount",
                           ),
-                          Text(
-                            _selectedDate == null
-                                ? "No Date Selected"
-                                : formatter.format(_selectedDate!),
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
-                  ],
-                ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              onPressed: _datePicker,
+                              icon: const Icon(Icons.calendar_month_outlined),
+                            ),
+                            Text(
+                              _selectedDate == null
+                                  ? "No Date Selected"
+                                  : formatter.format(_selectedDate!),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 const SizedBox(height: 5),
-                Row(
-                  children: [
-                    // const SizedBox(width: 5),
-                    DropdownButton(
-                        elevation: 8,
-                        borderRadius: BorderRadius.circular(4.0),
-                        hint: const Text("Type"),
-                        value: _selectedCategory,
-                        items: Category.values
-                            .map(
-                              (category) => DropdownMenuItem(
-                                  value: category,
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      Icon(categoryIcons[category]),
-                                      //const SizedBox(width: 4),
-                                      Text(
-                                        category.name.toUpperCase(),
-                                      ),
-                                    ],
-                                  )),
-                            )
-                            .toList(), //if the dropdown has a value selected it will change in set state
-                        onChanged: (value) {
-                          if (value == null) {
-                            return;
-                          }
-                          setState(() {
-                            _selectedCategory = value;
-                          });
-                        }),
-                    const Spacer(),
-                    ElevatedButton(
-                      onPressed: _addExpense,
-                      child: const Text("Add Expense"),
-                    )
-                  ],
-                ),
+                if (width >= 600)
+                  Row(
+                    children: [
+                      const Spacer(),
+                      // TextButton(
+                      //   onPressed: () {
+                      //     Navigator.pop(context);
+                      //   },
+                      //   child: const Text("Cancel"),
+                      // ),
+                      ElevatedButton(
+                        onPressed: _addExpense,
+                        child: const Text("Add Expense"),
+                      ),
+                    ],
+                  )
+                else
+                  Row(
+                    children: [
+                      // const SizedBox(width: 5),
+                      DropdownButton(
+                          elevation: 8,
+                          borderRadius: BorderRadius.circular(4.0),
+                          hint: const Text("Type"),
+                          value: _selectedCategory,
+                          items: Category.values
+                              .map(
+                                (category) => DropdownMenuItem(
+                                    value: category,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Icon(categoryIcons[category]),
+                                        //const SizedBox(width: 4),
+                                        Text(
+                                          category.name.toUpperCase(),
+                                        ),
+                                      ],
+                                    )),
+                              )
+                              .toList(), //if the dropdown has a value selected it will change in set state
+                          onChanged: (value) {
+                            if (value == null) {
+                              return;
+                            }
+                            setState(() {
+                              _selectedCategory = value;
+                            });
+                          }),
+                      const Spacer(),
+                      ElevatedButton(
+                        onPressed: _addExpense,
+                        child: const Text("Add Expense"),
+                      )
+                    ],
+                  ),
                 const SizedBox(height: 15),
                 TextButton(
                   onPressed: () {
